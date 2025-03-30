39f49235e74662d78c4a11d5a2196f67644d043d
Jungha Woo <jungha.woo@gmail.com>
2025-03-30 18:04:02 -0400

    First commit

--- geomapclip/__init__.py
+++
@@ -1,4 +0,0 @@
-from .model import GeoMapCLIP
-from .model import ImageEncoder
-from .model import LocationEncoder
-from .train import train
--- geomapclip/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .GeoMapCLIP import GeoMapCLIP
-from .image_encoder import ImageEncoder
-from .location_encoder import LocationEncoder
